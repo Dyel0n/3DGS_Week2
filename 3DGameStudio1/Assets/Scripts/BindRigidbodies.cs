28962bdf8a2bcf028b0df2f36bedfe19b70ba298
Dyel0n <DBurrell@mica.edu>
2017-02-10 04:12:31 -0500

    Everything?

---
+++ 3DGameStudio1/Assets/Scripts/BindRigidbodies.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BindRigidbodies : MonoBehaviour {
+
+    public LayerMask rigidbodyLayer;
+    public float breakForce = Mathf.Infinity;
+    public float breakTorque = Mathf.Infinity;
+
+	void Awake()
+    {
+        var colliders = Physics.OverlapSphere(transform.position, transform.localScale.x / 2, rigidbodyLayer.value);
+
+        for (int i = 0; i < colliders.Length-1; i++)
+        {
+            FixedJoint joint = colliders[i].gameObject.AddComponent<FixedJoint>();
+            joint.connectedBody = colliders[i + 1].gameObject.GetComponent<Rigidbody>();
+            joint.breakForce = breakForce;
+            joint.breakTorque = breakTorque;
+        }
+
+
+        Destroy(gameObject);
+    }
+}
---
+++ 3DGameStudio1/Assets/Scripts/RadialRigidbodyActivation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RadialRigidbodyActivation : MonoBehaviour {
+
+    public LayerMask activationLayer;
+    public float range = 5;
+    public float speed = 5;
+
+    void OnEnable()
+    {
+        var colliders = Physics.OverlapSphere(transform.position, range, activationLayer.value);
+
+        foreach(Collider c in colliders)
+        {
+            //print(c.gameObject.name)
+            //c.gameObject.GetComponent<Rigidbody>().isKinematic = false; OLD
+            var rb = c.gameObject.GetComponent<Rigidbody>();
+            var distance = Vector3.Distance(transform.position, c.gameObject.transform.position);
+            StartCoroutine(ActivateRigidbody(rb, distance / speed));
+
+        }
+    }
+
+
+    IEnumerator ActivateRigidbody(Rigidbody rb, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        rb.isKinematic = false;
+    }
+}
